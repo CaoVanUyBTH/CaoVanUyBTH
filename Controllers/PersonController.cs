0e2f6397a0b3ec4ab506836533897d5d17aa3895
cao-uy <caovanuy00@gmail.com>
2022-09-23 10:34:13 +0700

    2109

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -4,17 +4,37 @@ using CaoVanUyBTH.Models;
 namespace CaoVanUyBTH.Controllers
 {
     public class PersonController: Controller
-{
-    [HttpPost]
+   {
+    
     public IActionResult Index(Person std)
     {
         ViewBag.Ps = std.PersonID + "-"+ std.PersonName + "-"+ std.PersonBir + "-" + std.Address; 
-        return View();
+       return View();
     }
-    public IActionResult Create ()
-    {
-        return View();
-    }
-}
 
+        public IActionResult List()
+         {
+    
+    // khoi tao list Student
+           List<Person> listPerson = new List<Person>()
+          {
+             new Person {PersonID = 1, PersonName = "Nguyen Van A", PersonBir="22/03/2001", Address = "Hà Nội"},
+             new Person {PersonID = 2, PersonName = "Nguyen Van B", PersonBir="09/10/2001", Address = "Thanh Hóa"},
+             new Person {PersonID = 3, PersonName = "Nguyen Van C", PersonBir="01/12/2001", Address= "Nam Định"}
+         
+          };
+          ViewData["Persons"] = listPerson;
+          return View(listPerson);
+        }
+[HttpPost]
+        public IActionResult Create(Person std)
+        {
+            string mess = std.PersonID + "-";
+            mess += std.PersonName + "-";
+            mess += std.PersonBir + "-";
+            mess += std.Address;
+            ViewBag.Pers = mess;
+            return View();
+        }
+    }
 }
--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,21 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using CaoVanUyBTH.Models;
 
-namespace CaoVanUyBTH.Controllers
+namespace CaovanUyBTH.Controllers
 {
 
 public class StudentController: Controller
 {
     //các action
 
-[HttpPost]
- public IActionResult Index(Student std)
+
+ public IActionResult Index (Student std)
 {
     ViewBag.message = std.StudentID+"-"+std.StudentName+"-"+std.StudentAge   ;
     return View();
 }
-    
-public IActionResult Index()
+  
+public IActionResult List()
 {
     
     // khoi tao list Student
@@ -25,10 +25,10 @@ public IActionResult Index()
          new Student {StudentID = 2, StudentName = "Nguyen Van B", StudentAge = 18},
          new Student {StudentID = 3, StudentName = "Nguyen Van C", StudentAge = 18},
          new Student {StudentID = 4, StudentName = "Nguyen Van D", StudentAge = 18},
-         new Student {StudentID = 5, StudentName = "Nguyen Van E", StudentAge = 18},
+         new Student {StudentID = 5, StudentName = "Nguyen Van E", StudentAge = 18}
      };
      ViewData["Students"] = stdList;
-    return View();
+    return View(stdList);
 }
 [HttpPost]
  public IActionResult Create(Student std)
